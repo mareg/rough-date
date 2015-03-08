9fd30900084f4607281e880c92d9e1c63e8d2f8e
Marek Matulka <marek@matulka.net>
2015-03-08 23:34:43 +0000

    Use StringDateFormatter

--- src/RoughDate/RoughDate.php
+++ src/RoughDate/RoughDate.php
@@ -11,8 +11,8 @@
 
 namespace RoughDate;
 
-use RoughDate\Exception\UnrecognizedDateFormat;
 use RoughDate\Helper\StringDateNormalizer;
+use RoughDate\Helper\StringDateFormatter;
 
 final class RoughDate
 {
@@ -68,32 +68,8 @@ final class RoughDate
      */
     public function format($format = self::DEFAULT_DATE_FORMAT)
     {
-        if ($format != self::DEFAULT_DATE_FORMAT) {
-            return $this->formatDateToString($format);
-        }
+        $formatter = StringDateFormatter::fromString($this->date);
 
-        return $this->date;
-    }
-
-    /**
-     * @param string $format
-     *
-     * @return string
-     */
-    private function formatDateToString($format)
-    {
-        $dateParts = explode('-', $this->date);
-
-        if ('00' === $dateParts[1] && '00' === $dateParts[2]) {
-            $date = \DateTime::createFromFormat('Y', $dateParts[0]);
-            $format = preg_replace('/[^L|^Y|^y|^ |^\.|^\,]/', '', $format);
-        } elseif ('00' === $dateParts[2]) {
-            $date = \DateTime::createFromFormat('Y-m', join('-', array_slice($dateParts, 0, 2)));
-            $format = preg_replace('/[^F|^m|^M|^n|^t|^L|^Y|^y|^ |^\.|^\,]/', '', $format);
-        } else {
-            $date = \DateTime::createFromFormat(self::DEFAULT_DATE_FORMAT, $this->date);
-        }
-
-        return $date->format($format);
+        return $formatter->format($format);
     }
 }
