bb4330d89dfb9cef0e9e029de4c425fda9ba50b2
Marek Matulka <marek@matulka.net>
2015-03-08 22:53:44 +0000

    Use StringDateNormalizer

--- src/RoughDate/RoughDate.php
+++ src/RoughDate/RoughDate.php
@@ -12,6 +12,7 @@
 namespace RoughDate;
 
 use RoughDate\Exception\UnrecognizedDateFormat;
+use RoughDate\Helper\StringDateNormalizer;
 
 final class RoughDate
 {
@@ -37,7 +38,9 @@ final class RoughDate
      */
     public static function fromString($string)
     {
-        return new RoughDate(self::normalizeDateFromString($string));
+        $noramlizer = new StringDateNormalizer();
+
+        return new RoughDate($noramlizer->normalize($string));
     }
 
     /**
@@ -92,37 +95,5 @@ final class RoughDate
         }
 
         return $date->format($format);
-    }
-
-    /**
-     * @param string $input
-     *
-     * @throws \Exception
-     *
-     * @return string
-     */
-    private static function normalizeDateFromString($input)
-    {
-        if (preg_match('/^\d{4}[\-][0]{2}[\-][0]{2}$/', $input) || preg_match('/^\d{4}[\-]\d{2}[\-][0]{2}$/', $input)) {
-            return $input;
-        }
-
-        if (preg_match('/^\d{4}[\-|\/]\d{2}[\-|\/]\d{2}$/', $input) || preg_match('/^\d{1,2}\.? [a-zA-Z]{3} \d{4}$/', $input)) {
-            return (new \DateTime($input))->format('Y-m-d');
-        }
-
-        if (preg_match('/^\d{4}[\-|\/|\.]\d{2}[\-|\/|\.]\d{2}$/', $input)) {
-            return str_replace('.', '-', $input);
-        }
-
-        if (preg_match('/^[a-zA-Z]{3} \d{4}$/', $input)) {
-            return (new \DateTime($input))->format('Y-m') . '-00';
-        }
-
-        if (preg_match('/^\d{4}$/', $input)) {
-            return $input . '-00-00';
-        }
-
-        throw new UnrecognizedDateFormat($input);
     }
 }
